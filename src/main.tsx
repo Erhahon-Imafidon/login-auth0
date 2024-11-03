9c1188245d8be2c654df35f7977c7c3cf2737a3f
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-03 22:06:05 +0100

    Fixed the typescript error

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain: string = process.env.REACT_APP_AUTH0_DOMAIN || '';
+const clientId: string = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Auth0Provider domain={domain} clientId={clientId}  authorizationParams={{
+          redirect_uri: window.location.origin
+      }}>
+        <App />
+      </Auth0Provider>
   </StrictMode>,
 )
