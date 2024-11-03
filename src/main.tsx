0db5d773450f20e6a2c58eeef20dccfcf9ae5d84
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-03 21:36:17 +0100

    Installed the @types/node as a Dev dependency to prevent the process typescript error

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
